8ce0dbd91c904a6974733b64cd4ef31910cca4f9
Brad Parker <hi@bradparker.com>
2014-12-02 11:08:01 +1000

    Update 2014-12-02T01:07:52.876Z

---
+++ shared/javascripts/artwork.js
@@ -0,0 +1,61 @@
+(function (global) {
+  var canvas  = document.getElementById('artwork');
+  global.redhatBrandPattern = 'corporate';
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  function initArray(n) {
+    return Array.apply([], Array(n));
+  }
+
+  function gridPositions (colCount) {
+    return initArray(colCount).map(function (_elem, index) {
+      return (100 / colCount) * index;
+    });
+  }
+
+  function randomArrayElem (arr) {
+    return arr[getRandomInt(0, arr.length - 1)];
+  }
+
+  var artwork = {
+    gridPositions: gridPositions(10)
+  };
+
+  function createShape (i) {
+    var div = document.createElement('div');
+    div.classList.add('artwork-shape');
+    div.style.left    = randomArrayElem(artwork.gridPositions).toString() + '%';
+    div.style.top     = randomArrayElem(artwork.gridPositions.slice(1)).toString() + '%';
+    div.style.height  = randomArrayElem(artwork.gridPositions.slice(1)).toString() + '%';
+    div.style.width   = randomArrayElem(artwork.gridPositions.slice(1)).toString() + '%';
+    div.style.opacity = randomArrayElem([0.25, 0.5, 0.75]);
+    div.style.animationDuration = getRandomInt(1,3).toString() + 's';
+    canvas.appendChild(div);
+    div.classList.add('skew-animate-in');
+  }
+
+  function addPattern () {
+    var div = document.createElement('div');
+    div.setAttribute('data-red-hat-pattern', 'corporate');
+    div.classList.add('artwork-pattern');
+    div.style.animationDuration = getRandomInt(1,3).toString() + 's';
+    canvas.appendChild(div);
+    div.classList.add('rotate-animate-in');
+  }
+
+  function populateCanvas () {
+    for (var i = 0; i < 20; i++) {
+      createShape(i);
+    }
+
+    addPattern();
+  }
+  populateCanvas();
+})(window);
