9759878933e243ac80299afa26937148055363f4
Brad Parker <hi@bradparker.com>
2015-01-20 17:21:12 +1000

    [WIP] Switch agenda to use "sessions"
    No more wrangling flexible numbers of sessions / breakout sessions being
    treated as second class citizens

--- src/templates/agenda-board/assets/javascripts/main.js
+++ src/templates/agenda-board/assets/javascripts/main.js
@@ -1,5 +1,25 @@
 (function (global) {
-  var days = document.getElementsByClassName('section-agenda');
+  var __slice = [].slice;
+
+  var days = document.getElementsByClassName('time-table');
   var day = days[(global.redhatSelectedDay || 1) - 1];
   (day && day.style || {}).display = 'block';
+
+  (function initAgenda () {
+    var $radios = __slice.call(document.querySelectorAll('#agenda input[type="radio"]'));
+    var $labels = __slice.call(document.querySelectorAll('#agenda input[type="radio"] + label'));
+    var $timeTables = __slice.call(document.querySelectorAll('.time-table'));
+
+    $radios.forEach(function (radio, index) {
+      radio.setAttribute('id', 'day-' + index);
+    });
+
+    $labels.forEach(function (label, index) {
+      label.setAttribute('for', 'day-' + index);
+    });
+
+    $timeTables.forEach(function (table, index) {
+      table.classList.add('time-table-' + index);
+    });
+  })();
 })(window)
